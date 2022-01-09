ff086297383cdf47bf713a015f4c65c7aa2d4a7d
Eeshan8503 <Eeshan8503@users.noreply.github.com>
2022-01-09 19:32:22 +0530

    temp

--- client/Home/Welcome.js
+++ client/Home/Welcome.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import Navbar from '../Navbar/Navbar';
@@ -41,26 +40,6 @@ export default function Welcome({ navigation }) {
             Email: test@bial.app
           </Text>
           <Text
-=======
-import React from 'react'
-import { StyleSheet, Text, View,Image,TouchableOpacity } from 'react-native'
-import { LinearGradient } from 'expo-linear-gradient';
-export default function Welcome({navigation}) {
-    return (
-        <LinearGradient
-        // Background Linear Gradient
-        colors={['#44369E','#0B0733']}
-        style={{
-          height:'100%',
-          width:'100%',
-          alignItems: 'center',
-          
-        }}
-      >
-      <View style={styles.container}>
-            <Image
-            source={require("./../assets/pp.png")}
->>>>>>> d977927a6d774c8a828e8f8989a8bc4d0462847a
             style={{
               marginTop: 20,
               fontSize: 20,
@@ -81,7 +60,6 @@ export default function Welcome({navigation}) {
             alignContent: 'center',
             borderRadius: 25
           }}
-<<<<<<< HEAD
           onPress={() => navigation.navigate('Dashboard')}
         >
           <View>
@@ -94,18 +72,6 @@ export default function Welcome({navigation}) {
             >
               Start new Trip +
             </Text>
-=======
-          onPress={()=>navigation.navigate('Trip')}
-          >
-          <View>
-            <Text
-            style={{
-              textAlign:'center',
-              color:'white',
-              fontSize:25
-            }}
-            >Start new Trip +</Text>
->>>>>>> d977927a6d774c8a828e8f8989a8bc4d0462847a
           </View>
         </TouchableOpacity>
       </View>
